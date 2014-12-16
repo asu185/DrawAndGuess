9c398dfffd93d9f6f7058277e5a1b2f9b9c1c863
Marcin Peck <peck@student.chalmers.se>
2014-12-16 17:21:24 +0100

    Create application

---
+++ DrawAndGuess/src/com/game/drawandguess/StartScreenActivity.java
@@ -0,0 +1,15 @@
+package com.game.drawandguess;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class StartScreenActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_screen);
+    }
+}
